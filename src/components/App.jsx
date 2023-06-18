0eb660a0002c06f232f07d00ee75c30c57291f9e
Heorhii <gerythepatient@gmail.com>
2023-06-18 13:30:09 +0200

    Add movie details outlet

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import { Home } from "./pages/Home/Home";
 import { Movies } from "./pages/Movies/Movies";
 import { MovieDetails } from "./pages/MovieDetails/MovieDetails";
 import { NotFound } from "./pages/NotFound/NotFound";
+import { Cast } from "./Cast/Cast";
+import { Reviews } from "./Reviews/Reviews";
 import styled from "styled-components";
 
 const StyledLink = styled(NavLink)`
@@ -25,7 +27,10 @@ export const App = () => {
       <Routes>
          <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
-        <Route path="/movie/:movieId" element={<MovieDetails />} />
+        <Route path="/movie/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+          </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
--- src/components/pages/MovieDetails/MovieDetails.jsx
+++ src/components/pages/MovieDetails/MovieDetails.jsx
@@ -1,6 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link, Outlet } from "react-router-dom";
 
 export const MovieDetails = () => {
 const { movieId } = useParams();
-  return <div>Now showing product with id - {movieId}</div>;
+  return <div>
+    <h1>Now showing movie with id - {movieId}</h1>
+    <p>Additional information</p>
+    <ul>
+      <li><Link to="cast">Cast</Link></li>
+      <li>
+          <Link to="reviews">Reviews</Link>
+        </li>
+    </ul>
+     <Outlet />
+  </div>;
 };
