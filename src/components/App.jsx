cfc48052cc0fb4bc79cde5fc2b98ab6ad815ee0b
Heorhii <gerythepatient@gmail.com>
2023-06-18 15:24:57 +0200

    Add shared layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,25 @@
-import { Routes, Route, NavLink } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import { Movies } from "./pages/Movies/Movies";
 import { MovieDetails } from "./pages/MovieDetails/MovieDetails";
 import { NotFound } from "./pages/NotFound/NotFound";
 import { Cast } from "./Cast/Cast";
 import { Reviews } from "./Reviews/Reviews";
-import styled from "styled-components";
-
-const StyledLink = styled(NavLink)`
-  color: black;
-
-  &.active {
-    color: orange;
-  }
-`;
+import { SharedLayout } from "./SharedLayout/SharedLayout";
 
 export const App = () => {
   return (
     <div>
-      <nav>
-        <StyledLink to="/" end>
-          Home
-        </StyledLink>
-        <StyledLink to="/movies">Movies</StyledLink>
-      </nav>
       <Routes>
-         <Route path="/" element={<Home />} />
+          <Route path="/" element={<SharedLayout />}>
+         <Route index element={<Home />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/movie/:movieId" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
           </Route>
-        <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
+          </Route>
       </Routes>
     </div>
   );
---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import { Container, Header, Link } from "./SharedLayout.styled";
+
+export const SharedLayout = () => {
+  return (
+    <Container>
+      <Header>
+        <nav>
+        <Link to="/" end>
+          Home
+        </Link>
+        <Link to="/movies">Movies</Link>
+      </nav>
+      </Header>
+      <Outlet />
+    </Container>
+  );
+};
