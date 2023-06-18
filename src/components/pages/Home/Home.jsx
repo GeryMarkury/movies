762a9cadc0d8efab0f9663f47c9bf70e721ff7e8
Heorhii <gerythepatient@gmail.com>
2023-06-18 19:45:13 +0200

    Update Home.jsx

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,9 +1,33 @@
+import { fetchTrendingMovies } from "Api";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
 export const Home = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetchTrendingMovies();
+          setMovies(response.results);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
     return (
         <div>
         <h1>Trending today</h1>
         <ul>
-           <li>Film</li>
+           {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>{movie.title}
+          </Link>
+               </li>
+               ))}
             </ul>
             </div>
     )
