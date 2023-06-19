89699a8ff9ee21ece6723ac9e4296d030c8e7d97
Heorhii <gerythepatient@gmail.com>
2023-06-19 12:32:59 +0200

    Update MovieDetails page

---
+++ src/components/pages/MovieDetails/MovieDetail.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MovieContainer = styled.div`
+display: flex;
+gap: 30px;  
+max-width: 960px;
+  margin: 0 auto;
+  padding: 0 16px;
+`;
+
+export const Image = styled.img`
+width: 400px;
+`;
--- src/components/pages/MovieDetails/MovieDetails.jsx
+++ src/components/pages/MovieDetails/MovieDetails.jsx
@@ -1,10 +1,16 @@
 import { useParams, Link, Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { fetchMovieById } from "Api";
+import defaultPoster from '../../../images/defaultMoviePoster.jpg';
+import { MovieContainer, Image } from "./MovieDetail.styled";
 
 export const MovieDetails = () => {
   const { movieId } = useParams();
   const [movie, setMovie] = useState({});
+  
+  const BASE_URL = 'https://image.tmdb.org/t/p/';
+  const IMG_SIZE = 'w400';
+
   const genres = movie.genres ? movie.genres.map(genre => genre.name).join(", ") : "";
 
   useEffect(() => {
@@ -20,13 +26,14 @@ export const MovieDetails = () => {
     fetchData();
   }, [movie, movieId]);
     
-  return (<div>
-    <h2>{movie.title}</h2>
-    <p>User score: {movie.popularity}</p>
+  return (<>
+    <MovieContainer><div><Image src={movie.poster_path ? `${BASE_URL}${IMG_SIZE}${movie.poster_path}` : defaultPoster} alt={movie.title} /></div>
+    <div><h2>{movie.title}</h2>
+    <p>User score: {movie.vote_average}</p>
     <h3>Overview</h3>
     <p>{movie.overview}</p>
     <h4>Genres</h4>
-    {genres}
+    <p>{genres}</p></div></MovieContainer>
     <p>Additional information</p>
     <ul>
       <li><Link to="cast">Cast</Link></li>
@@ -35,5 +42,5 @@ export const MovieDetails = () => {
       </li>
     </ul>
     <Outlet />
-  </div>)
+  </>)
 };
