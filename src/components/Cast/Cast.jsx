329c317a8272e6e6d2f66f430b9dfef9019d3b0d
Heorhii <gerythepatient@gmail.com>
2023-06-19 13:05:32 +0200

    Add Cast and Reviews

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,1 +1,56 @@
-export const Cast = () => { };
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchCastOfMovie } from 'Api';
+import defaultPerson from '../../images/defaultPerson.png'
+
+const BASE_URL = 'https://image.tmdb.org/t/p/';
+const IMG_SIZE = 'w200';
+
+export const Cast = () => {
+    const [cast, setCast] = useState([]);
+    const { movieId } = useParams();
+
+    useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetchCastOfMovie(movieId);
+        setCast(response.cast);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    };
+
+    fetchData();
+    }, [movieId]);
+    
+    return (
+        <>
+        <ul>
+          {cast.length === 0 ? (
+            <p style={{ fontSize: '20px', fontWeight: 'bold' }}>
+              No information about the cast
+            </p>
+          ) : (
+            cast.map(({ id, name, character, profile_path }) => {
+              return (
+                <li key={id}>
+                  <div>
+                    <img
+                      src={
+                        profile_path
+                          ? `${BASE_URL}${IMG_SIZE}${profile_path}`
+                          : defaultPerson
+                      }
+                        alt={name}
+                        style={{ width: '200px' }}
+                    />
+                    <p>{name}</p>
+                    <p>Character: {character}</p>
+                  </div>
+                </li>
+              );
+            })
+          )}
+</ul></>
+    );
+};
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,1 +1,40 @@
-export const Reviews = () => { };
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchReviewsOfMovie } from 'Api';
+
+export const Reviews = () => { 
+    const [reviews, setReviews] = useState([]);
+    const { movieId } = useParams();
+
+    useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetchReviewsOfMovie(movieId);
+          setReviews(response.results);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    };
+
+    fetchData();
+    }, [movieId]);
+
+    return (
+        <ul>
+            {reviews.length === 0 ? (
+            <p style={{ fontSize: '20px', fontWeight: 'bold' }}>
+              We don't have any reviews for this movie
+            </p>
+          ) : (
+            reviews.map(({ id, author, content }) => {
+              return (
+                <li key={id}>
+                      <p style={{ fontSize: '20px', fontWeight: 'bold' }}>Author: {author}</p>
+                      <p>{content}</p>
+                </li>
+              );
+            })
+          )}
+        </ul>
+    )
+};
